776ff2461a3b630d941a31ef931ba4c9e3505050
Sean Goresht <seangores@gmail.com>
2016-08-06 02:48:16 -0600

    Reformat end-to-end tests to es2015

--- template/test/e2e/specs/test.js
+++ template/test/e2e/specs/test.js
@@ -1,14 +1,15 @@
-// For authoring Nightwatch tests, see
-// http://nightwatchjs.org/guide#usage
+// For authoring Nightwatch tests, see http://nightwatchjs.org/guide#usage
+
+"use strict";
 
 module.exports = {
-  'default e2e tests': function (browser) {
-    browser
-    .url('http://localhost:8080')
-      .waitForElementVisible('#app', 5000)
-      .assert.elementPresent('.logo')
-      .assert.containsText('h1', 'Hello World!')
-      .assert.elementCount('p', 3)
-      .end()
-  }
-}
+	"default e2e tests": (browser) => {
+		browser
+			.url("http://localhost:8080")
+			.waitForElementVisible("#app", 5000)
+			.assert.elementPresent(".logo")
+			.assert.containsText("h1", "Hello World!")
+			.assert.elementCount("p", 3)
+			.end();
+	}
+};
